58cbe3a5e9b5df44364f171050654937056f2ab7
Ehsanul Chowdhury <164161496+ehsanul-chowdhury@users.noreply.github.com>
2026-09-19 20:14:19 +0600

    Modify sieve.c to use user input for limit
    Updated sieve algorithm to accept user-defined limit.

--- lab_report_solutions_1/3_sieve.c
+++ lab_report_solutions_1/3_sieve.c
@@ -1,15 +1,25 @@
-#include<stdio.h>
-int main(){
-    int p[151],i,j;
-    for(i=2;i<151;i++)
-        p[i]=0;
-    i=2;
-    while(i<150){
-        if(p[i]==0)
-            printf("%d ",i);
-        for(j=1;i*j<=150;j++)
-            p[i*j]=1;
+#include <stdio.h>
+
+int main() {
+    int n, p[1000], i, j;
+
+    printf("Enter the limit: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i++)
+        p[i] = 0;
+
+    i = 2;
+
+    while(i <= n) {
+        if(p[i] == 0)
+            printf("%d ", i);
+
+        for(j = 1; i * j <= n; j++)
+            p[i * j] = 1;
+
         i++;
     }
+
     return 0;
 }
